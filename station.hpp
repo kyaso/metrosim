93dbb9614e3ed8b5dc12f82e7c907fb863c7d213
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2019-07-22 23:04:05 +0200

    Added RoutingTable to Station

--- station.hpp
+++ station.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
 #include "util.hpp"
+#include "routing_table.hpp"
 #include <string>
+#include <vector>
 
 class Station : public Object {
     private:
         Coord location;
 
     public:
+        RoutingTable routing_table;
+
         Station(unsigned int id, std::string name, int _x, int _y);
         ~Station() {DBG("Station destructor");}
         Coord loc();
